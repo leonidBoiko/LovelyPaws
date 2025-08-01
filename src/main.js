b92c2fe0ee5117cf2eac43f2989ecb5f8d61db4e
Leonid Boiko <boiko.w.leonid@gmail.com>
2025-08-01 19:29:00 +0300

    feat: add links; create collapsible content

--- src/main.js
+++ src/main.js
@@ -22,4 +22,12 @@ document.addEventListener('DOMContentLoaded', async () => {
   navigation();
   // Initialize Swiper after includes are loaded
   initializeSwiper();
+
+  const textBlock = document.getElementById('about-collapsible-content');
+  const toggleBtn = document.getElementById('about-collapsible-button');
+
+  toggleBtn.addEventListener('click', () => {
+    const isExpanded = textBlock.classList.toggle('about-expanded');
+    toggleBtn.textContent = isExpanded ? 'Show less' : 'Show more';
+  });
 });
