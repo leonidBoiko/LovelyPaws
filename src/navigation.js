94ff957014616e23aeba465eae1801660efaf0e8
Leonid Boiko <boiko.w.leonid@gmail.com>
2025-08-02 18:47:55 +0300

    feat: add bgc for navigation

--- src/navigation.js
+++ src/navigation.js
@@ -1,11 +1,15 @@
 function navigation() {
   const navbarToggler = document.getElementById('nav-toggler');
   const navbarMenu = document.getElementById('nav-menu');
+  const navBackground = document.getElementById('nav-bg');
   const links = navbarMenu.querySelectorAll('a');
+  const body = document.getElementsByTagName("body")[0];
 
   links.forEach(link => {
     link.addEventListener('click', event => {
       navbarMenu.classList.add('d-none');
+      navBackground.classList.add('d-none');
+      body.classList.remove('overflow-hidden');
     });
   });
 
@@ -14,8 +18,12 @@ function navigation() {
       if (navbar) {
         if (navbarMenu.classList.contains('d-none')) {
           navbarMenu.classList.remove('d-none');
+          navBackground.classList.remove('d-none');
+          body.classList.add('overflow-hidden');
         } else {
           navbarMenu.classList.add('d-none');
+          navBackground.classList.add('d-none');
+          body.classList.remove('overflow-hidden');
         }
       }
     });
